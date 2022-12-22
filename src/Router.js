e1d286f5d023efe632fc6473d1d460f2aef0180c
Kushan Dimantha <kushandimantha533@gmail.com>
2022-12-22 09:17:32 +0530

    multiple image upload demo success

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/layouts/LandingPage";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import ProductAdd from "./components/product/ProductAdd";
 
 const SiteRouter = () => {
 
@@ -11,7 +12,7 @@ const SiteRouter = () => {
     <div>
         <Router>
             <Routes>
-                <Route path="/" element={<LandingPage/>}/>
+                <Route path="/" element={<ProductAdd/>}/>
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/login" element={<Login/>}/>
             </Routes>
---
+++ src/components/product/ProductAdd.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Badge,
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Label,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Input,
+  Form,
+  Row,
+  Col,
+  CardImg,
+  Container,
+  CardText,
+} from "reactstrap";
+import moment from "moment/moment";
+import axios from "axios";
+
+
+const ProductAdd = () => {
+
+const [image, setImage] = useState([]);
+const [SKU, setSKU] = useState('#CA01');
+const [quantity, setquantity] = useState('10');
+const [productName, setproductName] = useState('product1');
+const [productDescription, setproductDescription] = useState('no description');
+
+console.log(image)
+
+const handleSubmit = () => {
+
+  let formData = new FormData()
+  formData.append('SKU', SKU);
+  formData.append('quantity', quantity);
+  formData.append('productName', productName);
+  formData.append('productDescription', productDescription);
+  Array.from(image).forEach(item => {
+    formData.append('products', item)
+  });
+  console.log("sending data set ",formData);
+  const url = 'http://localhost:5000/vendor-portal/product/add'
+  axios.post(url, formData).then(result => {
+    console.log("result",result)
+    if(result.data.status === 1)
+    {
+      alert('Product Added Successfully');
+      window.location.reload();
+    }
+    else
+    {
+      alert('Product Not Added');
+      window.location.reload();
+    }
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
+  return (
+    <div style={{margin:"100px"}}>
+     
+     {
+        Array.from(image).map((item,index) => {
+          return (
+            <span>
+              <img 
+                key={index}
+                style={{ padding: '10px' }}
+                width={150} height={100}
+                src={item ? URL.createObjectURL(item) : null} />
+            </span>
+          )
+        })
+      }
+
+      <Input
+        onChange={(e) => {
+          setImage(e.target.files)
+        }}
+        multiple
+        type="file"
+      />
+      <br/>
+      <Button className="btn btn-success" onClick={handleSubmit}> SUBMIT</Button>
+
+
+    </div>
+  );
+};
+
+export default ProductAdd;
